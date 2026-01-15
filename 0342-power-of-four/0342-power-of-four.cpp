14b9063ec3f98c430b24b171de2f2d1da4fb21a9
Samridhi Sehgal <sehgalsamridhi05@gmail.com>
2026-01-15 08:27:42 +0530

    Time: 0 ms (100%), Space: 7.8 MB (47.72%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,16 +1,11 @@
+#include <cmath>
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-    int ans=1;
-    for(int i=0;i<30;i++)
-    {
-        if(ans==n)
-        {
-            return true;
-        }
-     if(ans<INT_MAX/4)
-      
-        ans=ans*4;
+        if(n <= 0) return false;
+
+        double val = log2(n);         // log base 2
+        return fabs(val - round(val)) < 1e-10  // is integer
+               && ((int)round(val) % 2 == 0); // is even
     }
-    return false;}
 };
