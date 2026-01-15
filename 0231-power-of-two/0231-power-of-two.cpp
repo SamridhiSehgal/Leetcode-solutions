9ceed7e55c57d0ccf1d024001fcd7126c1dc7445
Samridhi Sehgal <sehgalsamridhi05@gmail.com>
2026-01-15 06:55:26 +0530

    Time: 0 ms (100%), Space: 8.1 MB (2.36%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,15 +1,10 @@
+#include <cmath>
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int ans=1;
-        for(int i=0;i<=30;i++)
-        {if(ans==n){
-            return true;
-        }
+        if (n <= 0) return false;
 
-            if(ans<INT_MAX/2)
-            {
-             ans=ans*2;
-            }
-        }return false;}
+        double val = log2(n);
+        return fabs(val - floor(val)) < 1e-10;
+    }
 };
