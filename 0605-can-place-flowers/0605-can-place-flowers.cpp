55e44d934e79fdacc1dfb63eeff87d3ca20a61e8
Samridhi Sehgal <sehgalsamridhi05@gmail.com>
2026-01-17 11:15:51 +0530

    Time: 0 ms (100%), Space: 24.2 MB (40.88%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -4,15 +4,17 @@ public:
         if (n == 0) {
             return true;
         }
-        for (int i = 0; i < flowerbed.size(); i++) {
-            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.size()-1 || flowerbed[i+1] == 0)) {
-                flowerbed[i] = 1;
-                n--;
-                if (n == 0) {
-                    return true;
-                }
+        for(int i=0;i<flowerbed.size();i++)
+        {
+            if(flowerbed[i]==0&&(i==0||flowerbed[i-1]==0)&&(i==flowerbed.size()-1||flowerbed[i+1]==0))
+            {
+                flowerbed[i]=1;
+                n=n-1;
             }
-        }
-        return false;
+        if(n==0)
+        {return true;}
+    }
+    return false;
+
     }
 };
