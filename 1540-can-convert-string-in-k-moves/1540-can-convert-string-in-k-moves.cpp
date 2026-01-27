c3660b9d4436d5c8913a00a2fa64cd1db778b1a5
Samridhi Sehgal <sehgalsamridhi05@gmail.com>
2026-01-27 23:06:30 +0530

    Time: 4 ms (86.36%), Space: 20.8 MB (92.42%) - LeetHub

--- 1540-can-convert-string-in-k-moves/1540-can-convert-string-in-k-moves.cpp
+++ 1540-can-convert-string-in-k-moves/1540-can-convert-string-in-k-moves.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     bool canConvertString(string s, string t, int k) {
-        if (s.size() != t.size()) return false;
+        int n = s.size();
+        if (n != t.size()) return false;
 
-        vector<int> count(26, 0);  // how many times each shift is needed
+        int j = 0;  // character index
+        int i = 1;  // move counter
+        vector<int> used(26, 0); // track how many times each shift has been used
 
-        for (int j = 0; j < s.size(); j++) {
+        while (j < n) {
             if (s[j] != t[j]) {
-                int shift = (t[j] - s[j] + 26) % 26;
-                if (shift == 0) continue;
+                int moves = (t[j] - s[j] + 26) % 26;
+                if (moves != 0) {
+                    // calculate the next allowed move for this shift
+                    int required = moves + used[moves] * 26;
 
-                count[shift]++;
-                int moves_needed = shift + (count[shift]-1)*26;
+                    // if the required move exceeds k, cannot convert
+                    if (required > k) return false;
 
-                if (moves_needed > k) return false;
+                    used[moves]++; // mark this shift as used
+                }
+
+                s[j] = t[j]; // convert the character
             }
+
+            j++; // move to next character
         }
 
         return true;
