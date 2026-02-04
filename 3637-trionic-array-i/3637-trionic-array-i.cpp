480d6ed27ef48fb66ae1a1950f8cb0c93f9eecf0
Samridhi Sehgal <sehgalsamridhi05@gmail.com>
2026-02-04 06:57:11 +0530

    Time: 0 ms (100%), Space: 27.8 MB (35.37%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.cpp
+++ 3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     bool isTrionic(vector<int>& nums) {
-        int n = nums.size();
-        if (n < 4) return false;
-
-        int i = 0;
-
-        // 1️⃣ strictly increasing
-        while (i + 1 < n && nums[i] < nums[i + 1]) {
-            i++;
-        }
-        if (i == 0 || i == n - 1) return false;
-
-        // 2️⃣ strictly decreasing
-        int decStart = i;
-        while (i + 1 < n && nums[i] > nums[i + 1]) {
-            i++;
-        }
-        if (i == decStart || i == n - 1) return false;
-
-        // 3️⃣ strictly increasing
-        while (i + 1 < n && nums[i] < nums[i + 1]) {
-            i++;
-        }
-
-        return i == n - 1;
+      int n=nums.size();
+      int i=0;
+      while(i+1<n&&nums[i]<nums[i+1])
+      {
+        i++;
+      }
+      if(i==0||i==n-1)
+      {
+        return false;
+      }
+      int dec=i;
+      while(i+1<n&&nums[i]>nums[i+1])
+      {
+        i++;
+      }
+      if(i==dec||i==n-1)
+      {
+        return false;
+      }
+      while(i+1<n&&nums[i]<nums[i+1])
+      {
+        i++;
+      }
+      if(i==n-1)
+      {
+        return true;
+      }
+      return false;
     }
 };
