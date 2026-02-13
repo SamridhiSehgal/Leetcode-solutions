d080ca36dce9acdd20dcd3a1ee23a2633e1075c5
Samridhi Sehgal <sehgalsamridhi05@gmail.com>
2026-02-13 11:34:40 +0530

    Time: 69 ms (93.42%), Space: 11.4 MB (94.89%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     int longestBalanced(string s) {
-        int n = s.size();
-        int maxLen = 0;
-
-        for(int i = 0; i < n; i++) {
-
-            unordered_map<char, int> mp;
-            int distinct = 0;
-            int maxFreq = 0;
-
-            for(int j = i; j < n; j++) {
-
-                if(mp[s[j]] == 0)
-                    distinct++;
-
-                mp[s[j]]++;
-                maxFreq = max(maxFreq, mp[s[j]]);
-
-                int len = j - i + 1;
-
-                if(len == distinct * maxFreq)
-                    maxLen = max(maxLen, len);
-            }
+        int n=s.size();int maxl=0;  
+        for(int i=0;i<n;i++){
+        int freq[26]={0};
+ 
+        for(int j=i;j<n;j++)
+        {
+            freq[s[j]-'a']++;
+        
+             int distinct=0;
+        int value=0;
+        bool valid=true;
+        for(int k=0;k<26;k++)
+        {
+            if(freq[k]>0)
+            {
+            if(value==0)
+            {
+                value=freq[k];
+            }else
+            if(freq[k]!=value)
+            {
+                valid=false;
+                break;
+            }}  
         }
-
-        return maxLen;
+        if(valid)
+        {
+            maxl=max(maxl,j-i+1);
+        }}}
+        return maxl;
     }
 };
