45f6e3484142745b2991864738325fe43afa4e8f
Samridhi Sehgal <sehgalsamridhi05@gmail.com>
2026-02-25 22:13:40 +0530

    Time: 7 ms (19.88%), Space: 14.3 MB (43.16%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -4,24 +4,21 @@ using namespace std;
 
 class Solution {
 public:
-
-    // Count number of 1 bits using your logic (& and >>)
     static int countBits(int n) {
         int count = 0;
         while(n > 0) {
-            count += (n & 1);   // check last bit
-            n = n >> 1;         // shift right
+            count += (n & 1);   
+            n = n >> 1;         
         }
         return count;
     }
 
-    // Comparator function
     static bool compare(int a, int b) {
         int countA = countBits(a);
         int countB = countBits(b);
 
         if(countA == countB)
-            return a < b;
+          return a < b;
 
         return countA < countB;
     }
